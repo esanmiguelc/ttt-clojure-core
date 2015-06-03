f770bbebb6e39305441c6630725f3662c23dc545
esanmiguelc <esanmiguel@gmail.com>
2015-06-03 15:14:04 -0500

    initial commit

---
+++ spec/ttt_clojure_core/core_spec.clj
@@ -0,0 +1,7 @@
+(ns ttt-clojure-core.core-spec
+  (:require [speclj.core :refer :all]
+            [ttt-clojure-core.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
